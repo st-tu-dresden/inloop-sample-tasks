998dee63ed8883d56838054f75f77e99afc4a900
Jonny Seitz <github@jonny-seitz.de>
2017-05-04 20:11:31 +0200

    Add exception test

--- fibonacci/tests/FibonacciTest.java
+++ fibonacci/tests/FibonacciTest.java
@@ -14,6 +14,16 @@ public class FibonacciTest {
     @Test
     public void testcalcFib() {
         assertEquals("The result of calcFib(0) should be 0.", 0, f.testcalcFib(0));
+        assertEquals("The result of calcFib(3) should be 2.", 2, f.testcalcFib(3));
+        assertEquals("The result of calcFib(6) should be 8.", 8, f.testcalcFib(6));
+
+        //test exception
+        try {
+            new f.testcalcFib(-1);
+            fail("Fibonacci.testcalcFib() should throw a IllegalArgumentException if the argument is negative!");
+        } catch (IllegalArgumentException e) {
+        }
+
     }
 
 }
