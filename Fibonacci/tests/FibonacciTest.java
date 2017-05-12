2f2b4a581038f879ca55901f11ddfbc3f6d443e9
Jonny Seitz <github@jonny-seitz.de>
2017-05-12 12:22:11 +0200

    Right method calling in test

--- Fibonacci/tests/FibonacciTest.java
+++ Fibonacci/tests/FibonacciTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,18 +9,23 @@ public class FibonacciTest {
 
     @Before
     public void setUp() {
-        f = new Fibonacci;
+        f = new Fibonacci();
     }
 
     @Test
     public void testcalcFib() {
-        assertEquals("The result of calcFib(0) should be 0.", 0, f.testcalcFib(0));
-        assertEquals("The result of calcFib(3) should be 2.", 2, f.testcalcFib(3));
-        assertEquals("The result of calcFib(6) should be 8.", 8, f.testcalcFib(6));
+        //test some test values
+        assertEquals("The result of calcFib(0) should be 0.", 0, f.calcFib(0));
+        assertEquals("The result of calcFib(3) should be 2.", 2, f.calcFib(3));
+        assertEquals("The result of calcFib(6) should be 8.", 8, f.calcFib(6));
+    }
 
+
+    @Test
+    public void testcalcFibIllegalArgument() {
         //test exception
         try {
-            new f.testcalcFib(-1);
+            f.calcFib(-1);
             fail("Fibonacci.testcalcFib() should throw a IllegalArgumentException if the argument is negative!");
         } catch (IllegalArgumentException e) {}
     }
